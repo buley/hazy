3159776c4bcaf742d9e015444293ba3dfaf8e6fe
Taylor Buley <taylor.buley@gmail.com>
2012-11-16 21:57:45 -0500

    fixup!

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,8 +1,17 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.makeKeyAndVisable
-    @window.rootViewController = HomeController.new
+    #navigationController = UINavigationController.alloc.initWithRootViewController(HomeController.new)
+    #@window.rootViewController = navigationController
+    tabController = UITabBarController.alloc.init
+    vc1 = GameController.alloc.initWithTabBar
+    vc2 = SettingsController.alloc.initWithTabBar 
+
+    # Adds controllers to tab bar
+    tabController.viewControllers = [ vc1, vc2 ]
+
+    @window.rootViewController = tabController
+    @window.makeKeyAndVisible
     true
   end
 end
--- app/home_controller.rb
+++ app/home_controller.rb
@@ -1,5 +1,35 @@
 class HomeController < UIViewController
 
+  def loadView
+    self.view = UIView.alloc.init
+  end
 
+  def viewDidLoad
+    self.title = "Home"
+    label = UILabel.alloc.initWithFrame([[15,100], [200,140]])
+    label.text = "This is my text"
+    label.font = UIFont.boldSystemFontOfSize(25)
+    label.textColor = UIColor.blackColor
+    label.textAlignment = UITextAlignmentCenter
+
+    # Initialize a button
+    button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    # Position the button
+    button.frame = [[15, 300], [200,50]]
+    # Sets the button title
+    button.setTitle("Move to next view", forState: UIControlStateNormal)
+    button.addTarget(self, action: "moveToChildView:", forControlEvents: UIControlEventTouchUpInside)
+
+    # Adds the label to the UIViewController
+    view.addSubview(label)
+
+    # Adds the button to the UIViewContoller
+    view.addSubview(button)
+
+  end
+
+  def moveToChildView(sender)
+    self.parentViewController.pushViewController(UIViewController.alloc.init, animated: true)
+  end
 
 end
